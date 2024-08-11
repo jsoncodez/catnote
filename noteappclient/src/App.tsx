cf98e036040eefdef0a69d7bb116f95eed9cd203
jsoncodez <jason20@umbc.edu>
2024-08-11 12:14:49 -0400

    added note click selection function, added save and cancel buttons when note is clicked

--- noteappclient/src/App.tsx
+++ noteappclient/src/App.tsx
@@ -4,6 +4,11 @@ import {useState} from "react";
 import logo from './logo.svg';
 import './App.css';
 
+type Note = {
+  id: number;
+  title: string;
+  content: string;
+}
 
 function App() {
   const [notes, setNotes] = useState([
@@ -13,26 +18,89 @@ function App() {
       content: "content 1"
     },
     {
-      id:1,
+      id:2,
       title: "note title 2",
       content: "content 2"
     },
     {
-      id:1,
+      id:3,
       title: "note title 3",
       content: "content 3"
     },
     {
-      id:1,
+      id:4,
       title: "note title 4",
       content: "content 4"
     },
     {
-      id:1,
+      id:5,
       title: "note title 5",
       content: "content 5"
     },
-  ])
+  ]);
+
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [selectedNote, setSelectedNote] = useState<Note | null>(null);
+
+  const handleNoteClick = (note:Note) => {
+    setSelectedNote(note);
+    setTitle(note.title);
+    setContent(note.content);
+  };
+
+
+  const handleSubmit = (event: React.FormEvent)=>{
+
+    event.preventDefault();
+    console.log("title: ", title);
+    console.log("content: ", content);
+
+    const newNote: Note = {
+      id: notes.length + 1,
+      title: title,
+      content: content, 
+    };
+    setNotes([newNote, ...notes]);
+    setTitle("");
+    setContent("");
+  };
+
+  const handleUpdateNote = (
+    event: React.FormEvent 
+  )=>{
+    event.preventDefault();
+
+    if(!selectedNote) {
+      return;
+    }
+
+    const updatedNote: Note = {
+      id: selectedNote.id,
+      title: title,
+      content: content,
+
+    }
+
+    const updatedNotesList = notes.map((note)=>
+      note.id === selectedNote.id
+        ? updatedNote
+        : note
+      )
+    
+    setNotes(updatedNotesList)
+    setTitle("")
+    setContent("")
+    setSelectedNote(null);
+  
+  };
+
+  const handleCancel=() => {
+    setTitle("")
+    setContent("")
+    setSelectedNote(null);
+  }
+
   return (
     
     <div className="App">
@@ -45,30 +113,56 @@ function App() {
 
 
       <div className = "App-container">
-        <form className="note-form">
+        <form className="note-form"
+          onSubmit={(event)=> 
+            selectedNote
+              ? handleUpdateNote(event)
+              : handleSubmit(event)
+          // onSubmit={(event)=>handleSubmit(event)}>
+          }>
+
           <input
+            value = {title}
+            onChange={(event)=>
+            setTitle(event.target.value)
+            }
             placeholder='title'
             required
           ></input>
           
           <textarea
+
+            value={content}
+            onChange={(event) =>
+              setContent(event.target.value)
+            }
             placeholder="Content"
             rows={10}
             required></textarea>
-            <button type="submit">Add Note</button>
+
+            {selectedNote ? (
+              <div className="edit-buttons">
+
+                <button type="submit">Save</button>
+                <button onClick={handleCancel}>Cancel</button>
+              </div>
+            ) : (
+              <button type="submit">Add Note</button>
+            )}
+            
         </form>
         <div className="notes-grid">
           {notes.map((note)=> (
-             <div className="note-item">
-             <div className="notes-header">
-               <button>x</button>
-             </div>
-             <h2>{note.title}</h2>
-             <p>{note.content}</p>
+            <div 
+              className="note-item"
+              onClick={() => handleNoteClick(note)}>
+            <div className="notes-header">
+              <button>x</button>
+            </div>
+            <h2>{note.title}</h2>
+            <p>{note.content}</p>
              
            </div>
-
-
 
           ))}
          
